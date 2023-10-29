235cbe2e617ed9bb486fec990ef4bce4dcfcfe5f
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2023-10-29 23:48:58 +0100

    Initial commit

---
+++ Microservices/WebApplication1/Data/PrepDb.cs
@@ -0,0 +1,50 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var seviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(seviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name=".Net",
+                        Publisher="Microsoft",
+                        Cost="120"
+                    },
+                    new Platform()
+                    {
+                        Name = ".Sql",
+                        Publisher = "Seequel",
+                        Cost = "20"
+                    },
+                    new Platform()
+                    {
+                        Name = "Kubernetes",
+                        Publisher = "Cloud Native",
+                        Cost = "120"
+                    }
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data in our database");
+            }
+
+        }
+
+    }
+}
